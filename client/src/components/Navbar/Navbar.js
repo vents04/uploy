c52842f6ee3b7dac6e42fd62829100f0b2c82bce
vents04 <vencidim04@gmail.com>
2022-04-02 00:44:14 +0300

    navbar init

---
+++ client/src/components/Navbar/Navbar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class Navbar extends Component {
+  render() {
+    return (
+      <div className="navbar-container">
+            <div className="navbar-logo-container">
+                <p className="navbar-logo"></p>
+            </div>
+      </div>
+    )
+  }
+}
