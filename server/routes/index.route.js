97c85ded3a8546f3d5b0709b738ed196ce0b88de
vents04 <vencidim04@gmail.com>
2022-04-02 01:36:38 +0300

    ride post endpoint init

--- server/routes/index.route.js
+++ server/routes/index.route.js
@@ -3,8 +3,10 @@ const router = express.Router();
 
 const userRoute = require('./user.route');
 const lenderRoute = require('./lender.route');
+const rideRoute = require('./ride.route');
 
 router.use('/user', userRoute);
 router.use('/lender', lenderRoute);
+router.use('/ride', rideRoute);
 
 module.exports = router;
