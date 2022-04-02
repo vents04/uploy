29c841c4f58950ce2bdeda76cdd59f0c205380ab
sv4t <73824869+sv4t@users.noreply.github.com>
2022-04-02 03:57:30 +0300

    vehicle post endpoint

--- server/routes/index.route.js
+++ server/routes/index.route.js
@@ -4,9 +4,11 @@ const router = express.Router();
 const userRoute = require('./user.route');
 const lenderRoute = require('./lender.route');
 const rideRoute = require('./ride.route');
+const vehicleRoute = require('./vehicle.route');
 
 router.use('/user', userRoute);
 router.use('/lender', lenderRoute);
 router.use('/ride', rideRoute);
+router.use('/vehicle', vehicleRoute);
 
 module.exports = router;
---
+++ server/routes/vehicle.route.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const { HTTP_STATUS_CODES, COLLECTIONS, DEFAULT_ERROR_MESSAGE, LENDER_STATUSES } = require('../global');
+const { postVehicleValidation, updateVehicleValidation } = require('../validation/hapi');
+const Vehicle = require('../db/models/vehicle.model');
+const router = express.Router();
+
+const { authenticate } = require('../middlewares/authenticate');
+
+const ResponseError = require('../errors/responseError');
+const DbService = require('../services/db.service');
+const AuthenticationService = require('../services/authentication.service');
+
+router.post("/", async (req, res, next) => {
+    const { error } = postVehicleValidation(req.body);
+    if (error) return next(new ResponseError(error.details[0].message, HTTP_STATUS_CODES.BAD_REQUEST));
+
+    const lender = await DbService.getById(COLLECTIONS.LENDERS, req.body.lenderId);
+    if(!lender && lender.status != LENDER_STATUSES.ACTIVE){
+        return next(new ResponseError("Lender wasn't found or his status wasn't active", HTTP_STATUS_CODES.BAD_REQUEST));
+    }
+    try {
+        const vehicle = new Vehicle(req.body);
+        await DbService.create(COLLECTIONS.VEHICLES, vehicle);
+
+    } catch (err) {
+        return next(new ResponseError(err.message || DEFAULT_ERROR_MESSAGE, err.status || HTTP_STATUS_CODES.INTERNAL_SERVER_ERROR));
+    }
+});
+
+module.exports = router;
