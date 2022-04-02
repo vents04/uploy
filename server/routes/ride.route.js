e56ecde23e1f36476d7b009664f6b6760d493059
PancakeB0Y <nikola.goev@gmail.com>
2022-04-02 03:37:42 +0300

    ride.route added

--- server/routes/ride.route.js
+++ server/routes/ride.route.js
@@ -1,5 +1,6 @@
 const express = require("express");
-const { COLLECTIONS, HTTP_STATUS_CODES } = require("../global");
+const Ride = require("../db/models/ride.model");
+const { COLLECTIONS, HTTP_STATUS_CODES, RIDE_STATUSES } = require("../global");
 const { authenticate } = require("../middlewares/authenticate");
 const DbService = require("../services/db.service");
 const { postRideValidation } = require("../validation/hapi");
@@ -15,6 +16,16 @@ router.post('/', authenticate, async(req, res, next) => {
         if(vehicle.lenderId.toString() == req.user._id.toString()) return next(new ResponseError("Cannot rent your own vehicles", HTTP_STATUS_CODES.CONFLICT));
 
         const ride = new Ride(req.body);
+        const rides = await DbService.getOne(COLLECTIONS.RIDES, {vehicleId: req.body.vehicleId, "$and": [
+            {status: {"$ne": RIDE_STATUSES.CANCELLED}},
+            {status: {"$ne": RIDE_STATUSES.FINISHED}},
+        ]});
+
+        for(let current of rides){
+            if(current != ride){
+                             
+            }
+        }
         ride.userId = req.user._id.toString();
         await DbService.create(COLLECTIONS.RIDES, ride);
 
