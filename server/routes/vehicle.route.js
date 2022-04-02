7424cc58e6a7bbdf2ef63638cd057a0581ea27f2
sv4t <73824869+sv4t@users.noreply.github.com>
2022-04-03 01:40:30 +0300

    pull

--- server/routes/vehicle.route.js
+++ server/routes/vehicle.route.js
@@ -87,7 +87,7 @@ router.post("/vehicle/search", async (req, res, next) => {
             for(let ride of rides){
                 if(!(req.body.pdt - THIRTY_MINUTES_IN_MILLISECONDS > ride.plannedReturnDt) 
                 && !(req.body.rdt + THIRTY_MINUTES_IN_MILLISECONDS < ride.plannedPickupDt)
-                && distance > 20){
+                && distance > 20 && req.user._id.toString() == vehicle.lenderId.toString()){
                     continue;
                 }
             }
