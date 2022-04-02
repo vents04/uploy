fa74057ba35d2c95cc79242cd633c3f930fa4f42
vents04 <vencidim04@gmail.com>
2022-04-02 13:11:44 +0300

    post vehicle design added

---
+++ client/src/screens/PostVehicle/PostVehicle.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+import { IoMdClose } from 'react-icons/io'
+
+import './PostVehicle.scss';
+
+export default class PostVehicle extends Component {
+  render() {
+    return (
+      <div className="top-bar">
+          <IoMdClose size={24} className="icon" />
+          <p className="top-bar-text">Add a vehicle</p>
+      </div>
+    )
+  }
+}
