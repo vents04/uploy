39c2092a87c37902bd70d47fd265b29307353700
PancakeB0Y <nikola.goev@gmail.com>
2022-04-02 16:50:58 +0300

    StripeService integration

--- server/services/stripe.service.js
+++ server/services/stripe.service.js
@@ -5,15 +5,21 @@ const { COLLECTIONS, HTTP_STATUS_CODES } = require('../global');
 const { STRIPE_SECRET_KEY, STRIPE_PUBLIC_KEY } = require('../global.js');
 const stripe = require('stripe')(STRIPE_SECRET_KEY);
 
-const vehicle = await stripe.products.create({name: 'Vehicle'});
-
 const StripeService = {
-    getPrice: function (collection, filter) {
+    getPrice: function (vehicle, price) {
         return new Promise(async (resolve, reject) => {
             validateCollection(collection, reject);
-            db.collection(collection).findOne(filter).then(resolve).catch((error) => {
-                reject(new ResponseError(error.message, HTTP_STATUS_CODES.INTERNAL_SERVER_ERROR));
+            const newVehicle = await stripe.products.create({name: vehicle.title});
+            const newPrice = await stripe.prices.create({
+                product: newVehicle.id,
+                unit_amount: price.amount,
+                currency: price.currency
             });
+            try {
+                resolve(newPrice)
+            } catch (error) {
+                reject(new ResponseError(error.message, HTTP_STATUS_CODES.INTERNAL_SERVER_ERROR));
+            }
         })
     }
 }
