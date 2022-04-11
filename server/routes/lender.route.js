defa9ec7a452a871b2f6e017448be090a5b9a543
vents04 <vencidim04@gmail.com>
2022-04-11 21:29:36 +0300

    lender route updated for admin use

--- server/routes/lender.route.js
+++ server/routes/lender.route.js
@@ -36,21 +36,41 @@ router.post("/", authenticate, async (req, res, next) => {
     }
 });
 
+router.put("/", authenticate, async (req, res, next) => {
+    const { error } = lenderUpdateValidation(req.body);
+    if (error) return next(new ResponseError(error.details[0].message, HTTP_STATUS_CODES.BAD_REQUEST));
+
+    try {
+
+        const lender = await DbService.getOne(COLLECTIONS.LENDERS, { userId: mongoose.Types.ObjectId(req.user._id) });
+        if (!lender) return next(new ResponseError("Lender not found", HTTP_STATUS_CODES.NOT_FOUND));
+
+        if (!req.isAdmin) {
+            if (lender.status == LENDER_STATUSES.BLOCKED || lender.status == LENDER_STATUSES.PENDING_APPROVAL)
+                return next(new ResponseError("Cannot perform lender status update when lender status is pending approval or blocked", HTTP_STATUS_CODES.CONFLICT));
+            if (req.body.status == LENDER_STATUSES.BLOCKED || req.body.status == LENDER_STATUSES.PENDING_APPROVAL)
+                return next(new ResponseError("Cannot perform lender status update with new status being pending approval or blocked", HTTP_STATUS_CODES.BAD_REQUEST));
+        }
+
+        await DbService.update(COLLECTIONS.LENDERS, { userId: mongoose.Types.ObjectId(req.user._id) }, req.body);
+
+        return res.sendStatus(HTTP_STATUS_CODES.OK);
+    } catch (err) {
+        return next(new ResponseError(err.message || DEFAULT_ERROR_MESSAGE, err.status || HTTP_STATUS_CODES.INTERNAL_SERVER_ERROR));
+    }
+});
+
 router.put("/:id", authenticate, async (req, res, next) => {
-    if (!mongoose.Types.ObjectId.isValid(req.params.id)) return next(new ResponseError("Invalid lender id", HTTP_STATUS_CODES.BAD_REQUEST));
+    if (!req.isAdmin) return next(new ResponseError("Only admins may update lenders", HTTP_STATUS_CODES.FORBIDDEN));
 
     const { error } = lenderUpdateValidation(req.body);
     if (error) return next(new ResponseError(error.details[0].message, HTTP_STATUS_CODES.BAD_REQUEST));
 
     try {
-        const lender = await DbService.getOne(COLLECTIONS.LENDERS, { userId: mongoose.Types.ObjectId(req.user._id) });
+        const lender = await DbService.getById(COLLECTIONS.LENDERS, req.params.id);
         if (!lender) return next(new ResponseError("Lender not found", HTTP_STATUS_CODES.NOT_FOUND));
-        if (lender.status == LENDER_STATUSES.BLOCKED || lender.status == LENDER_STATUSES.PENDING_APPROVAL)
-            return next(new ResponseError("Cannot perform lender status update when lender status is pending approval or blocked", HTTP_STATUS_CODES.CONFLICT));
-        if (req.body.status == LENDER_STATUSES.BLOCKED || req.body.status == LENDER_STATUSES.PENDING_APPROVAL)
-            return next(new ResponseError("Cannot perform lender status update with new status being pending approval or blocked", HTTP_STATUS_CODES.BAD_REQUEST));
 
-        await DbService.update(COLLECTIONS.LENDERS, { userId: mongoose.Types.ObjectId(req.user._id) }, req.body);
+        await DbService.update(COLLECTIONS.LENDERS, { _id: mongoose.Types.ObjectId(req.params.id) }, req.body);
 
         return res.sendStatus(HTTP_STATUS_CODES.OK);
     } catch (err) {
@@ -88,9 +108,62 @@ router.post("/stripe/account-link/:accountLinkType", authenticate, async (req, r
     }
 });
 
+router.post("/:id/stripe/account-link/:accountLinkType", authenticate, async (req, res, next) => {
+    if (!req.isAdmin) return next(new ResponseError("Only admins may create Stripe account links for lenders", HTTP_STATUS_CODES.FORBIDDEN));
+
+    if (!Object.values(STRIPE_ACCOUNT_LINK_TYPES).includes(req.params.accountLinkType)) return next(new ResponseError("Invalid stripe account link type", HTTP_STATUS_CODES.BAD_REQUEST));
+
+    try {
+        const lender = await DbService.getById(COLLECTIONS.LENDERS, req.params.id);
+        if (!lender) return next(new ResponseError("Lender not found", HTTP_STATUS_CODES.NOT_FOUND));
+
+        const stripeAccount = await DbService.getOne(COLLECTIONS.STRIPE_ACCOUNTS, { lenderId: mongoose.Types.ObjectId(lender._id) });
+        if (!stripeAccount) return next(new ResponseError("Stripe account not found", HTTP_STATUS_CODES.NOT_FOUND));
+
+        const accountLink = await StripeService.createAccountLink(stripeAccount.stripeAccountId, STRIPE_ACCOUNT_LINK_TYPES.ONBOARDING);
+        const stripeAccountLink = new StripeAccountLink({
+            stripeAccountLink: {
+                expires_at: accountLink.expires_at,
+                url: accountLink.url
+            },
+            stripeAccountLinkType: req.params.accountLinkType,
+            lenderId: lender._id
+        });
+        await DbService.create(COLLECTIONS.STRIPE_ACCOUNT_LINKS, stripeAccountLink);
+
+        return res.status(HTTP_STATUS_CODES.OK).send({
+            stripeAccountLink: stripeAccountLink
+        })
+    } catch (err) {
+        return next(new ResponseError(err.message || DEFAULT_ERROR_MESSAGE, err.status || HTTP_STATUS_CODES.INTERNAL_SERVER_ERROR));
+    }
+});
+
 router.get("/stripe/account", authenticate, async (req, res, next) => {
     try {
         const lender = await DbService.getOne(COLLECTIONS.LENDERS, { userId: mongoose.Types.ObjectId(req.user._id) });
+        if (!lender) return next(new ResponseError("Lender not found", HTTP_STATUS_CODES.NOT_FOUND));
+
+        const stripeAccount = await DbService.getOne(COLLECTIONS.STRIPE_ACCOUNTS, { lenderId: mongoose.Types.ObjectId(lender._id) });
+        if (!stripeAccount) return next(new ResponseError("Stripe account not found", HTTP_STATUS_CODES.NOT_FOUND));
+
+        // check the value of the details_submitted property to know if the lender has completed the onboarding process
+        const stripeAccountInstance = await StripeService.retrieveAccount(stripeAccount.stripeAccountId);
+
+        return res.status(HTTP_STATUS_CODES.OK).send({
+            stripeAccount: stripeAccount,
+            stripeAccountInstance: stripeAccountInstance
+        })
+    } catch (err) {
+        return next(new ResponseError(err.message || DEFAULT_ERROR_MESSAGE, err.status || HTTP_STATUS_CODES.INTERNAL_SERVER_ERROR));
+    }
+});
+
+router.get("/:id/stripe/account", authenticate, async (req, res, next) => {
+    if (!req.isAdmin) return next(new ResponseError("Only admins may get Stripe accounts of lenders", HTTP_STATUS_CODES.FORBIDDEN));
+
+    try {
+        const lender = await DbService.getById(COLLECTIONS.LENDERS, req.params.id);
         if (!lender) return next(new ResponseError("Lender not found", HTTP_STATUS_CODES.NOT_FOUND));
 
         const stripeAccount = await DbService.getOne(COLLECTIONS.STRIPE_ACCOUNTS, { lenderId: mongoose.Types.ObjectId(lender._id) });
