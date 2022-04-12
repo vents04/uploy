b5701cdd1bd5b7c861ed523cfa4975581fc4f7bd
vents04 <vencidim04@gmail.com>
2022-04-12 11:00:36 +0300

    added vehicle delete enpoint with admin use

--- server/routes/vehicle.route.js
+++ server/routes/vehicle.route.js
@@ -96,7 +96,27 @@ router.get("/", authenticate, async (req, res, next) => {
             vehicles
         });
     } catch (err) {
-        return next(new ResponseError(err.message || "Internal server error", err.status || HTTP_STATUS_CODES.INTERNAL_SERVER_ERROR));
+        return next(new ResponseError(err.message || DEFAULT_ERROR_MESSAGE, err.status || HTTP_STATUS_CODES.INTERNAL_SERVER_ERROR));
+    }
+});
+
+router.delete("/:id", authenticate, async (req, res, next) => {
+    if (!mongoose.Types.ObjectId.isValid(req.params.id)) return next(new ResponseError("Invalid vehicle id", HTTP_STATUS_CODES.BAD_REQUEST));
+
+    try {
+        const vehicle = await DbService.getById(COLLECTIONS.VEHICLES, req.params.id);
+        if (!vehicle) return next(new ResponseError("Vehicle not found", HTTP_STATUS_CODES.NOT_FOUND));
+
+        const lender = await DbService.getById(COLLECTIONS.LENDERS, vehicle.lenderId);
+        if (!lender) return next(new ResponseError("Lender not found", HTTP_STATUS_CODES.NOT_FOUND));
+        if (!req.isAdmin && lender.userId.toString() != req.user._id.toString()) return next(new ResponseError("You are not the owner of this vehicle", HTTP_STATUS_CODES.FORBIDDEN));
+
+        await DbService.delete(COLLECTIONS.VEHICLES, { _id: mongoose.Types.ObjectId(req.params.id) });
+        await DbService.delete(COLLECTIONS.KEYS, { vehicleId: mongoose.Types.ObjectId(req.params.id) });
+
+        return res.sendStatus(HTTP_STATUS_CODES.OK);
+    } catch (err) {
+        return next(new ResponseError(err.message || DEFAULT_ERROR_MESSAGE, err.status || HTTP_STATUS_CODES.INTERNAL_SERVER_ERROR));
     }
 });
 
